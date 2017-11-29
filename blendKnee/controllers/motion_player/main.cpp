af3e57810354813f7b39982ad758e909579d7aba
hannah <hannahchen0709@gmail.com>
2017-11-29 15:22:22 -0500

    upload whole folder

---
+++ blendKnee/controllers/motion_player/main.cpp
@@ -0,0 +1,17 @@
+// Description:   Manage the entree point function
+
+#include "MotionPlayer.hpp"
+
+#include <cstdlib>
+
+using namespace webots;
+
+int main(int argc, char **argv)
+{
+  MotionPlayer *controller = new MotionPlayer();
+  controller->textToSpeech();
+  controller->run();
+  delete controller;
+  return EXIT_FAILURE;
+  
+}
